7355a7e0dfd8fbad81e06afdfa424132738f892f
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-03-02 18:54:52 -0600

    added all the article pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,6 +42,26 @@ const routes: Routes = [
   {
     path: 'user-create',
     loadChildren: () => import('./user-create/user-create.module').then( m => m.UserCreatePageModule)
+  },
+  {
+    path: 'articles',
+    loadChildren: () => import('./articles/articles.module').then( m => m.ArticlesPageModule)
+  },
+  {
+    path: 'article',
+    loadChildren: () => import('./article/article.module').then( m => m.ArticlePageModule)
+  },
+  {
+    path: 'article-create',
+    loadChildren: () => import('./article-create/article-create.module').then( m => m.ArticleCreatePageModule)
+  },
+  {
+    path: 'article-edit',
+    loadChildren: () => import('./article-edit/article-edit.module').then( m => m.ArticleEditPageModule)
+  },
+  {
+    path: 'article-delete',
+    loadChildren: () => import('./article-delete/article-delete.module').then( m => m.ArticleDeletePageModule)
   }
 ];
 
